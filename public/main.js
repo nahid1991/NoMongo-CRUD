c056e14ddfd3e6e07dd7fc7725cebeadc3f9d312
Nahid Shaiket <nahidshaiket10300@gmail.com>
2016-06-17 11:57:11 +0600

    crud done

--- public/main.js
+++ public/main.js
@@ -11,3 +11,22 @@ update.addEventListener('click', function () {
     })
   })
 })
+
+var del = document.getElementById('delete')
+
+del.addEventListener('click' ,function(){
+    fetch('quotes', {
+        method: 'delete',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify({
+            'name': 'Darth Vader'
+    })
+    }).then(res => {
+    if(res.ok) return res.json()
+}).then(data => {
+    console.log(data)
+    window.location.reload()
+})
+})
